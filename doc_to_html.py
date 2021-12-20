905dd95aedc2b5c35a096d9c8a8a27d213d31900
quidampepin <pepin.david@gmail.com>
2021-12-20 14:59:16 -0500

    tests

--- doc_to_html.py
+++ doc_to_html.py
@@ -1,5 +1,5 @@
 import mammoth
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 f = open("test_code.docx", 'rb')
 b = open('page.html', 'wb')
 document = mammoth.convert_to_html(f)
@@ -10,5 +10,28 @@ b.close()
 with open("page.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
+with open("start.html") as fp:
+    soup1 = BeautifulSoup(fp, 'html.parser')
+
+
+with open("end.html") as fp:
+    soup2 = BeautifulSoup(fp, 'html.parser')
+
 with open("output1.html", "w") as file:
     file.write(str((soup.prettify())))
+
+
+with open("output2.html", "w") as file:
+    file.write(str((soup1.prettify())+(soup.prettify())+(soup2.prettify())))
+
+
+
+with open("output2.html", "r") as file:
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('<main class="container" property="mainContentOfPage" resource="#wb-main" typeof="WebPageElement">\n  </main>\n </body>\n</html>', '<main class="container" property="mainContentOfPage" resource="#wb-main" typeof="WebPageElement">\n')
+
+# Write the file out again
+with open('output2.html', 'w') as file:
+  file.write(filedata)
