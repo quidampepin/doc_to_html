b60a10b4f6f128277a0298d96478ac9520d9f156
Pepin <pepin.david@gmail.com>
2021-12-22 09:53:35 -0500

    Adding comments

--- doc_to_html.py
+++ doc_to_html.py
@@ -1,5 +1,7 @@
 import mammoth
 from bs4 import BeautifulSoup
+
+#convert word doc to an html file
 f = open("test_code_2.docx", 'rb')
 b = open('doc.html', 'wb')
 document = mammoth.convert_to_html(f)
@@ -7,25 +9,29 @@ b.write(document.value.encode('utf8'))
 f.close()
 b.close()
 
+#parse the html created from the word doc
 with open("doc.html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
+#parse the stable beginning of a Canada.ca page (header, menu)
 with open("start.html") as fp:
     soup1 = BeautifulSoup(fp, 'html.parser')
 
 
+#parse the stable end of a Canada.ca page (pre-footer and footer)
 with open("end.html") as fp:
     soup2 = BeautifulSoup(fp, 'html.parser')
 
+#prettify the html from word doc
 with open("doc_output.html", "w") as file:
     file.write(str((soup.prettify())))
 
-
+#combine the header, main and footer sections into a prettified html page
 with open("page.html", "w") as file:
     file.write(str((soup1.prettify())+(soup.prettify())+(soup2.prettify())))
 
 
-
+#clean up the generated html page into filedata
 with open("page.html", "r") as file:
     filedata = file.read()
     filedata = filedata.replace('</meta>\n </head>\n', '</head>\n')
@@ -43,8 +49,9 @@ with open("page.html", "r") as file:
     filedata = filedata.replace('<p>\n </details>\n</p>\n', '</details>\n')
 
 
-
-
-# Write the file out again
+#write the cleaned up filedata to the html page
 with open('page.html', 'w') as file:
   file.write(filedata)
+
+
+ #the generated HTML cam be copied in a gc-proto GitHub repo and create a full Canada.ca page
