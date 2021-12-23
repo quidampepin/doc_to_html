89d4c22172e87e6c0994e9ef9e1d896d09b6fe20
quidampepin <pepin.david@gmail.com>
2021-12-23 09:08:32 -0500

    testing page

--- doc_to_html.py
+++ doc_to_html.py
@@ -4,19 +4,28 @@ from bs4 import BeautifulSoup
 
 custom_styles = """ i => cite
                     p[style-name='ordered'] => ol
-                    p[style-name='details'] => details
+                    p[style-name='details'] => details:fresh
                     p[style-name='btn-cta'] => button.btn-call-to-action
                     p[style-name='btn-primary'] => button.btn-primary
                     p[style-name='btn-secondary'] => button.btn-default
+                    p[style-name='btn-danger'] => button.btn-danger
                     p[style-name='alert-danger'] => section.alert-danger > h2
                     p[style-name='alert-warning'] => section.alert-warning > h2
+                    p[style-name='alert-info'] => section.alert-info > h2
+                    p[style-name='alert-success'] => section.alert-success > h2
+                    p[style-name='label-default'] => span.label-default
+                    p[style-name='label-primary'] => span.label-primary
+                    p[style-name='label-success'] => span.label-success
+                    p[style-name='label-info'] => span.label-info
+                    p[style-name='label-warning'] => span.label-warning
+                    p[style-name='label-danger'] => span.label-danger
                     p[style-name='alert-text'] => alert > p
                     p[style-name='summary'] => summary"""
 
 
 #convert word doc to an html file
 
-with open("test_code.docx", "rb") as docx_file:
+with open("test_code_4.docx", "rb") as docx_file:
     result = mammoth.convert_to_html(docx_file, style_map = custom_styles)
     text = result.value
     with open('doc.html', 'w') as html_file:
@@ -57,16 +66,26 @@ with open("page.html", "r") as file:
     filedata = filedata.replace('<div class="par iparys_inherited">\n</div>\n<div class="par iparys_inherited">\n', '</main>\n<div class="par iparys_inherited">\n</div>\n<div class="par iparys_inherited">\n')
     filedata = filedata.replace('\n </a>\n ', '</a>')
     filedata = filedata.replace('<h3>\n On this page\n</h3>', '<h2 class="h3">\n On this page\n</h2>')
-    filedata = filedata.replace('</summary>\n<details>\n', '</summary>\n<p>\n')
-    filedata = filedata.replace('\n</details>', '\n</p>\n</details>')
-    filedata = filedata.replace('\n<summary>', '\n<details>\n<summary>')
+    filedata = filedata.replace('<summary>\n', '<details>\n<summary>\n')
+    filedata = filedata.replace('<details>\n end\n</details>\n', '</details>\n')
     filedata = filedata.replace('class="btn-call-to-action', 'class="btn btn-call-to-action')
     filedata = filedata.replace('class="btn-primary', 'class="btn btn-primary')
     filedata = filedata.replace('class="btn-default', 'class="btn btn-default')
+    filedata = filedata.replace('class="btn-danger', 'class="btn btn-danger')
     filedata = filedata.replace('class="alert-danger', 'class="alert alert-danger')
     filedata = filedata.replace('class="alert-warning', 'class="alert alert-warning')
+    filedata = filedata.replace('class="alert-info', 'class="alert alert-info')
+    filedata = filedata.replace('class="alert-success', 'class="alert alert-success')
     filedata = filedata.replace('</section>\n<alert>\n', '')
     filedata = filedata.replace('</alert>', '</section>')
+    filedata = filedata.replace('<table>', '<table class="provisional gc-table table" id="myTable1">')
+    filedata = filedata.replace('class="label-default', 'class="label label-default')
+    filedata = filedata.replace('class="label-primary', 'class="label label-primary')
+    filedata = filedata.replace('class="label-success', 'class="label label-success')
+    filedata = filedata.replace('class="label-info', 'class="label label-info')
+    filedata = filedata.replace('class="label-warning', 'class="label label-warning')
+    filedata = filedata.replace('class="label-danger', 'class="label label-danger')
+
 
 
 #write the cleaned up filedata to the html page
