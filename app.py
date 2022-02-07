6323871aada6e8694dbfc82f7dba07f1fdc2a056
quidampepin <pepin.david@gmail.com>
2022-02-07 09:35:04 -0500

    testing code

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask import render_template
 import mammoth
 from bs4 import BeautifulSoup
 from werkzeug.utils import secure_filename
+import pandas as pd
+import nltk
 
 app = Flask(__name__)
 
@@ -264,8 +266,177 @@ def html_convert():
     html_page = BeautifulSoup(filedata, "html.parser")
     aem_page = BeautifulSoup(filedata3, "html.parser")
 
+
+    # calculate readability score
+    nltk.download('punkt')
+    if lang == 'en':
+        word_column_names = ['Count', 'Word']
+
+    if lang == 'fr':
+        word_column_names = ['Nombre', 'Mot']
+
+    original_soup = BeautifulSoup(combined_page, features="lxml").find('main')
+
+    original_text = original_soup.get_text()
+    original_text = original_text.replace('..', '.')
+    original_text = original_text.replace('.', '. ')
+    original_text = original_text[:original_text.find("defPreFooter")]
+    original_text = original_text.replace('\n', '')
+    original_text = original_text.replace('\t', '')
+    original_text = original_text.replace('\r', '')
+
+    #get initial readability total_score
+    from readability import Readability
+    r_o = Readability(original_text)
+    original_fk = r_o.flesch_kincaid()
+    original_score = original_fk.score
+    original_score = format(original_score, '.2f')
+
+    #add periods after bullet points and headings so that the Flesch Kicaid score considers them as sentences
+    html1 = html.replace("</li>", ".</li>")
+    html2 = html1.replace("</h1>", ".</h1>")
+    html3 = html2.replace("</h2>", ".</h2>")
+    html4 = html3.replace("</h3>", ".</h3>")
+    html5 = html4.replace("</h4>", ".</h4>")
+    html6 = html5.replace("</h5>", ".</h5>")
+    html7 = html6.replace("</h6>", ".</h6>")
+
+    #get adjusted readability total_score
+    revised_soup = BeautifulSoup(html7, features="lxml").find('main')
+    for t in revised_soup.select('table'):
+        t.extract()
+    revised_text = revised_soup.get_text()
+    revised_text = revised_text.replace('..', '.')
+    revised_text = revised_text .replace('.', '. ')
+    revised_text = revised_text[:revised_text.find("defPreFooter")]
+    revised_text = revised_text.replace('\n', '')
+    revised_text = revised_text.replace('\t', '')
+    revised_text = revised_text.replace('\r', '')
+
+    from readability import Readability
+    r_f = Readability(revised_text)
+    final_fk = r_f.flesch_kincaid()
+
+
+    #tokenize the text for processing
+
+    from nltk.tokenize import RegexpTokenizer
+    tokenizer = RegexpTokenizer('\w+')
+    tokens = tokenizer.tokenize(revised_text)
+    words = []
+    for word in tokens:
+        words.append(word.lower())
+
+    #remove stop words from the tokens to get only the meaningful words
+    nltk.download('stopwords')
+    sw_en = nltk.corpus.stopwords.words('english')
+    words_ns_en = []
+    for word in words:
+        if word not in sw_en:
+            words_ns_en.append(word)
+
+    #get the 15 most used words in the text
+    from nltk import FreqDist
+    fdist1_en = FreqDist(words_ns_en)
+    most_common_en = fdist1_en.most_common(20)
+    mc_en = pd.DataFrame(most_common_en, columns =['Word', 'Count'])
+    mc_en = mc_en[['Count', 'Word']]
+
+
+    sw_fr = nltk.corpus.stopwords.words('french')
+    words_ns_fr = []
+    for word in words:
+        if word not in sw_fr:
+            words_ns_fr.append(word)
+
+    #get the 15 most used words in the text
+    from nltk import FreqDist
+    fdist1_fr = FreqDist(words_ns_fr)
+    most_common_fr = fdist1_fr.most_common(20)
+    mc_fr = pd.DataFrame(most_common_fr, columns =['Mot', 'Nombre'])
+
+
+
+    #get all headings and calculate how many words on average between headings
+    headings = original_soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'])
+    len_headings = len(headings)
+    hratio = len(words)/(len(headings))
+
+    #get all paragraphs and all bulleted list, and calculate how many words per paragraph on average
+    paragraphs = original_soup.findAll(['p', 'ul'])
+    len_par = len(paragraphs)
+    pratio = (len(words)/len(paragraphs))
+
+    #calculate points for readability
+    if final_fk.score <= 6:
+        fkpoints = 60
+    elif final_fk.score >= 18:
+        fkpoints = 0
+    else :
+        fkpoints = (60-((final_fk.score-6)*5))
+
+    #calculate points for number of words between headings
+    if hratio <= 40:
+        hpoints = 20
+    elif hratio >= 200:
+        hpoints = 0
+    else :
+        hpoints = (20-((hratio-40)*0.125    ))
+
+    #calculate points for number of words per paragraph
+    if pratio <= 30:
+        ppoints = 20
+    elif pratio >= 80:
+        ppoints = 0
+    else :
+        ppoints = (20-((pratio-30)*0.4))
+
+    #add all points
+    total_score = fkpoints+hpoints+ppoints
+    total_score = format(total_score, '.2f')
+    fkpoints = format(fkpoints, '.2f')
+    final_fk_score = format(final_fk.score, '.2f')
+    hpoints = format(hpoints, '.2f')
+    hratio = format(hratio, '.2f')
+    ppoints = format(ppoints, '.2f')
+    pratio = format(pratio, '.2f')
+    total_words = len(words)
+
+    total_score = float(total_score)
+    if total_score >= 90:
+        if lang=='en':
+            score = 'Outstanding!'
+        if lang=='fr':
+            score = 'Excellent!'
+    elif total_score >= 80 and total_score < 90:
+        if lang=='en':
+            score = 'Very good!'
+        if lang=='fr':
+            score = 'Très bien!'
+    elif total_score >= 70 and total_score < 80:
+        if lang=='en':
+            score = 'Not too bad'
+        if lang=='fr':
+            score = 'Pas mal'
+    elif total_score >= 60 and total_score < 70:
+        if lang=='en':
+            score = 'Needs work'
+        if lang=='fr':
+            score = 'À travailler'
+    elif total_score >= 50 and total_score < 60:
+        if lang=='en':
+            score = 'Needs a lot of work'
+        if lang=='fr':
+            score = 'Besoin de beaucoup de travail'
+    elif total_score < 50:
+        if lang=='en':
+            score = "Please don't do this to people..."
+        if lang=='fr':
+            score = "S'il vous plaît, il faut faire quelque chose..."
+
+
     return render_template(
-        f"code_{lang}.html", lang=lang, html_page=html_page, aem_page=aem_page
+        f"code_{lang}.html", lang=lang, html_page=html_page, aem_page=aem_page, total_score = total_score, fkpoints = fkpoints, final_fk_score = final_fk_score, hpoints = hpoints, hratio = hratio, ppoints = ppoints, pratio = pratio, total_words = total_words, url = url, word_column_names = word_column_names, row_data_word_en = list(mc_en.values.tolist()), row_data_word_fr = list(mc_fr.values.tolist()), zip = zip, score = score, len_headings = len_headings, len_par = len_par, original_score = original_score
     )
 
 
